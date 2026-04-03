029e5c2f9e297e1d819366ad41a93b54bbeccac9
Maira057 <maira.auge@gmx.ch>
2026-04-03 16:58:04 +0200

    changes

---
+++ Among-US-Group/timer.py
@@ -0,0 +1,14 @@
+From datetime import datetime, timedelta
+
+
+def is_seat_available (check_in_time):
+    now = datetime.now()
+    expiry = check_in_time + datetime.timedelta (hours=2)
+    if check_in_time == None:
+        return True
+    if now > expiry:
+        return True
+    else:
+        return False
+    
+
