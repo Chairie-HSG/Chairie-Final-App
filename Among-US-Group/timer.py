fb43f487fc6c4a9b202991de4d985b3f6a6b86eb
Maira057 <maira.auge@gmx.ch>
2026-04-10 13:10:36 +0200

    more changes

--- Among-US-Group/timer.py
+++ Among-US-Group/timer.py
@@ -3,14 +3,30 @@ From datetime import datetime, timedelta
 
 def has_expired (check_in_time):
 
-"""calculates when the 2 hours end"""
     expiry_time = check_in_time + timedelta(hours=2)
+    """calculates when the 2 hours end"""
 
-"""gets the current time"""
     current_time = datetime.now()
+    """gets the current time"""
 
-"""check if current time has passed the expiry time"""
     if current_time > expiry_time:
+        """check if current time has passed the expiry time"""
         return True
     else:
-        return false
+        return False
+
+def free_expired_seats(seats):
+
+    for seat in seats:
+        """go through every seat"""
+
+        if seat ['occupied']:
+            """only check seats that are occupied"""
+
+            check_in_time = datetime.fromisoformat(seat['check_in_time'])
+
+            if has_expired (check_in_time):
+                """if 2 hours passed, free the seat"""
+
+                seat['occupied'] = False
+                seat ['check_in_time'] = None
