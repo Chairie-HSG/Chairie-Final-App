a9b7e5bf6970e9bd844d671cea5561063486ad87
KishibeLunatik <99767445+KishibeLunatik@users.noreply.github.com>
2026-04-23 18:01:01 +0200

    Update supabase_client.py

--- supabase_client.py
+++ supabase_client.py
@@ -1,11 +1,15 @@
  #connect python to the database
-from dotenv import load_dotenv
 import os
+import streamlit as st
+from dotenv import load_dotenv
 from supabase import create_client
 
 load_dotenv()
 
-SUPABASE_URL = os.getenv("SUPABASE_URL")
-SUPABASE_KEY = os.getenv("SUPABASE_KEY")
+SUPABASE_URL = st.secrets.get("SUPABASE_URL", os.getenv("SUPABASE_URL"))
+SUPABASE_KEY = st.secrets.get("SUPABASE_KEY", os.getenv("SUPABASE_KEY"))
+
+if not SUPABASE_URL or not SUPABASE_KEY:
+    raise ValueError("Missing SUPABASE_URL or SUPABASE_KEY")
 
 supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
