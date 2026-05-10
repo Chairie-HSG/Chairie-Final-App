4bad434554503ecbafc1c268b89bd683c10ee8d0
Maira057 <maira.auge@gmx.ch>
2026-05-10 12:09:50 +0200

    testing

---
+++ test_qr_code.py
@@ -0,0 +1,12 @@
+import streamlit at st
+from qr_code import show_checkin
+
+def fake_check_in(token, seat_id):
+    return {"success": True, "message": f"Checked in to seat {seat_id}!"}
+
+show_checkin(
+    token="fake_token",
+    expected_seat_id=42,
+    expected_seat_code="A-14",
+    check_in_fn=fake_check_in,
+)
